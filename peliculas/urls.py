b0784fc8d9c31cf704f8bfadbadb27bef9237b2e
Andres.14 <feliperin14@hotmail.es>
2023-10-30 14:42:28 -0500

    se realiza la integracion de la API para la busqueda de Peliculas (vista y url Buscar_peliculas)

--- peliculas/urls.py
+++ peliculas/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
 
@@ -11,4 +10,5 @@ urlpatterns = [
   path('peliculas/agregar', views.agregar, name='agregar'),
   path('peliculas/editar/<int:id>', views.editar, name='editar'),
   path('eliminar/<int:id>', views.eliminar, name='eliminar'),
+  path('buscar', views.buscar_peliculas, name='buscar_peliculas')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- peliculas/views.py
+++ peliculas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+import requests
 from .models import Pelicula
 from .forms import PeliculaForm
 
@@ -12,6 +13,20 @@ def ayuda(request):
 def peliculas(request):
   peliculas = Pelicula.objects.all()
   return render(request, 'peliculas/index.html', {'peliculas':peliculas})
+
+def buscar_peliculas(request):
+  if request.method == "POST":
+    termino_busqueda = request.POST.get("termino_busqueda")
+    api_key = "85efd76158e2b80a6a3d456beb14f93c"
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={termino_busqueda}"
+    response = requests.get(url)
+    if response.status_code == 200:
+      datos = response.json()
+      resultados = datos.get("results")
+    else:
+      resultados = []
+    return render(request, "peliculas/buscar_peliculas.html", {"resultados": resultados})
+  return render(request, "peliculas/buscar_peliculas.html")
 
 def agregar(request):
   formulario = PeliculaForm(request.POST or None, request.FILES or None)
